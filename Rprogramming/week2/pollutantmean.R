bb0b8c29f5949d7caac96f955f492e574ec796b8
heikalm <heikalm@gmail.com>
2019-03-12 10:18:31 -0400

    Commit for pollutantmean function

---
+++ Rprogramming/week2/pollutantmean.R
@@ -0,0 +1,6 @@
+pollutantmean <- function(directory, pollutant, id){
+        files_full <- list.files(directory, full.names=TRUE) #get the full file path
+        files_full_subset <- files_full[id] #subset of only the relevant ids
+        dat <- do.call(rbind, lapply(files_full_subset, read.csv)) #read from all the csv files of relevant IDs into dataframe
+        mean(dat[,pollutant], na.rm=TRUE) #calculate the mean of the pollutant, excluding NA values
+}

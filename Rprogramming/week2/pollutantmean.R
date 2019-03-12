6c1a30e4d28e28414a79bda195c8695382966525
heikalm <heikalm@gmail.com>
2019-03-12 10:22:14 -0400

    added id default value of 1:332

--- Rprogramming/week2/pollutantmean.R
+++ Rprogramming/week2/pollutantmean.R
@@ -1,4 +1,4 @@
-pollutantmean <- function(directory, pollutant, id){
+pollutantmean <- function(directory, pollutant, id=1:332){
         files_full <- list.files(directory, full.names=TRUE) #get the full file path
         files_full_subset <- files_full[id] #subset of only the relevant ids
         dat <- do.call(rbind, lapply(files_full_subset, read.csv)) #read from all the csv files of relevant IDs into dataframe

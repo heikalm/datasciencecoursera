5ff57a7291ffcee5011b949098d3b4d2efb364b5
heikalm <heikalm@gmail.com>
2019-03-12 12:05:16 -0400

    Commit for complete function

---
+++ Rprogramming/week2/complete.R
@@ -0,0 +1,12 @@
+complete <- function(directory, id=1:332) {
+        files_full <- list.files(directory, full.names=TRUE) #get the full file path
+        files_full_subset <- files_full[id] #subset of only the relevant ids
+        dat <- do.call(rbind, lapply(files_full_subset, read.csv)) #read from all the csv files of relevant IDs into dataframe
+        result <- data.frame()
+        for (i in unique(dat[,"ID"]))
+                result <- rbind(result, c(i, sum(complete.cases(dat[dat[,"ID"] == i,]))))
+                #print(c(i, sum(complete.cases(dat[dat[,"ID"] == i,]))))
+        colnames(result) <- c("id", "nobs")
+        result        
+               
+}

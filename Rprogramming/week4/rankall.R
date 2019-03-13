5a12a950b3501c7d801af748787536c02149027b
heikalm <heikalm@gmail.com>
2019-03-13 14:51:28 -0400

    commit for Programming assignment coursera answers

---
+++ Rprogramming/week4/rankall.R
@@ -0,0 +1,36 @@
+rankall <- function(outcome, num = "best") {
+        outcomedata <- read.csv("rprog-data-ProgAssignment3-data/outcome-of-care-measures.csv", colClasses = "character")## Read outcome data
+        
+        validstates <- unique(outcomedata$State) #generate list of valid states
+        validoutcomes <- c("heart attack", "heart failure", "pneumonia") #generate list of outcomes
+        
+        if (!(outcome %in% validoutcomes)) stop ("invalid outcome") # stop if outcome is not valid
+        
+        outcomecol <- c(11, 17, 23)
+        columnmap <- data.frame(validoutcomes, outcomecol)
+        outcomecolno <- columnmap[match(outcome, columnmap[,"validoutcomes"]),"outcomecol"] #map the outcome input to the relevant column in the data
+        
+        result <- data.frame()
+        
+        for (state in validstates[order(validstates)]) {
+                stateoutcomedata <- outcomedata[outcomedata$State == state & outcomedata[,outcomecolno] != "Not Available",] #subset the data to the relevant state, only hospitals with data
+                ordereddata <- stateoutcomedata[order(as.numeric(stateoutcomedata[,outcomecolno]),stateoutcomedata[,2]),] #order the data in ascending order by outcome rate, then Hospital name
+                if (num == "best") {num <- 1}
+                if (num == "worst") {
+                        result <- rbind(result,data.frame("Hospital.Name"=tail(ordereddata[,"Hospital.Name"],1),"State"=state) )
+                } else {
+                        result <- rbind(result, data.frame("Hospital.Name"=ordereddata[num,"Hospital.Name"],"State"=state) )
+                        
+                        }
+                
+                
+                
+        }
+        row.names(result) <- result$State
+        return(result)
+}
+
+
+## For each state, find the hospital of the given rank
+## Return a data frame with the hospital names and the
+## (abbreviated) state name
---
+++ Rprogramming/week4/rankhospital.R
@@ -0,0 +1,23 @@
+rankhospital <- function(state, outcome, num = "best") {
+        outcomedata <- read.csv("rprog-data-ProgAssignment3-data/outcome-of-care-measures.csv", colClasses = "character")## Read outcome data
+        
+        validstates <- unique(outcomedata$State) #generate list of valid states
+        validoutcomes <- c("heart attack", "heart failure", "pneumonia") #generate list of outcomes
+        
+        if (!(state %in% validstates)) stop ("invalid state") # stop if state is not valid
+        if (!(outcome %in% validoutcomes)) stop ("invalid outcome") # stop if outcome is not valid
+        
+        outcomecol <- c(11, 17, 23)
+        columnmap <- data.frame(validoutcomes, outcomecol)
+        outcomecolno <- columnmap[match(outcome, columnmap[,"validoutcomes"]),"outcomecol"] #map the outcome input to the relevant column in the data
+        
+        stateoutcomedata <- outcomedata[outcomedata$State == state & outcomedata[,outcomecolno] != "Not Available",] #subset the data to the relevant state
+        ordereddata <- stateoutcomedata[order(as.numeric(stateoutcomedata[,outcomecolno]),stateoutcomedata[,2]),] #order the data in ascending order by outcome rate, then Hospital name
+        if (num == "best") {num <- 1}
+        if (num == "worst") {return(tail(ordereddata[,"Hospital.Name"],1))
+                } else {
+                        return(ordereddata[num,"Hospital.Name"])}
+}
+## Check that state and outcome are valid
+## Return hospital name in that state with the given rank
+## 30-day death rate
